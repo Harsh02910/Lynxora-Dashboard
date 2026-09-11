b02086aa36e88fdd036d2b46507f968e6f10e153
Harsh02910 <harshadalja029@gmail.com>
2026-09-11 12:18:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  Lynxora PWA â€” Service Worker (Network-First)
 //  Always fetches latest files, falls back to offline cache
 // â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•â•
-const CACHE_NAME = 'lynxora-v25.1';
+const CACHE_NAME = 'lynxora-v25.2';
 const OFFLINE_ASSETS = [
   './',
   './index.html',
